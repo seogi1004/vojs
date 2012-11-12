3f0cbb14f2efe2c7714e76270f8922ec55033a1d
seogi1004 <seogi1004@naver.com>
2012-11-12 11:56:21 +0900

    underscore.js dependency remove!! 1.2.0

--- vo.js
+++ vo.js
@@ -1,6 +1,109 @@
 /*! vojs v@1.1.0 | seogi1004.github.com/vojs */
 
 (function(experts) {
+	var ViewObjectTpl = function(text, data, settings) {
+		var _ = {};
+	
+		var ArrayProto = Array.prototype;
+		var slice = ArrayProto.slice;
+		var nativeForEach = ArrayProto.forEach;
+		
+		var escapes = {
+			'\\' : '\\',
+			"'" : "'",
+			'r' : '\r',
+			'n' : '\n',
+			't' : '\t',
+			'u2028' : '\u2028',
+			'u2029' : '\u2029'
+		};
+	
+		for (var p in escapes)
+		escapes[escapes[p]] = p;
+		var escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
+		var unescaper = /\\(\\|'|r|n|t|u2028|u2029)/g;
+	
+		var unescape = function(code) {
+			return code.replace(unescaper, function(match, escape) {
+				return escapes[escape];
+			});
+		};
+	
+		var each = _.each = _.forEach = function(obj, iterator, context) {
+			if (obj == null)
+				return;
+			if (nativeForEach && obj.forEach === nativeForEach) {
+				obj.forEach(iterator, context);
+			} else if (obj.length === +obj.length) {
+				for (var i = 0, l = obj.length; i < l; i++) {
+					if ( i in obj && iterator.call(context, obj[i], i, obj) === breaker)
+						return;
+				}
+			} else {
+				for (var key in obj) {
+					if (_.has(obj, key)) {
+						if (iterator.call(context, obj[key], key, obj) === breaker)
+							return;
+					}
+				}
+			}
+		};
+	
+		var noMatch = /.^/;
+		
+		_.has = function(obj, key) {
+			return hasOwnProperty.call(obj, key);
+		};
+	
+		_.defaults = function(obj) {
+			each(slice.call(arguments, 1), function(source) {
+				for (var prop in source) {
+					if (obj[prop] == null)
+						obj[prop] = source[prop];
+				}
+			});
+			return obj;
+		};
+	
+		_.templateSettings = {
+			evaluate : /<%([\s\S]+?)%>/g,
+			interpolate : /<%=([\s\S]+?)%>/g,
+			escape : /<%-([\s\S]+?)%>/g
+		};
+	
+		_.template = function(text, data, settings) {
+			settings = _.defaults(settings || {}, _.templateSettings);
+	
+			var source = "__p+='" + text.replace(escaper, function(match) {
+				return '\\' + escapes[match];
+			}).replace(settings.escape || noMatch, function(match, code) {
+				return "'+\n_.escape(" + unescape(code) + ")+\n'";
+			}).replace(settings.interpolate || noMatch, function(match, code) {
+				return "'+\n(" + unescape(code) + ")+\n'";
+			}).replace(settings.evaluate || noMatch, function(match, code) {
+				return "';\n" + unescape(code) + "\n;__p+='";
+			}) + "';\n";
+	
+			if (!settings.variable)
+				source = 'with(obj||{}){\n' + source + '}\n';
+	
+			source = "var __p='';" + "var print=function(){__p+=Array.prototype.join.call(arguments, '')};\n" + source + "return __p;\n";
+	
+			var render = new Function(settings.variable || 'obj', '_', source);
+			if (data)
+				return render(data, _);
+			var template = function(data) {
+				return render.call(this, data, _);
+			};
+	
+			template.source = 'function(' + (settings.variable || 'obj') + '){\n' + source + '}';
+	
+			return template;
+		};
+		
+		return _.template(text, data, settings);
+	}
+	
 	var ViewObject = function(id) {
 		var	self 		= this;
 			self.root 	= (id) ? $("#" + id).get(0) : $("body").get(0),
@@ -168,7 +271,7 @@
 					}
 					
 					//  tpl 갱신
-					sel.html(_.template($tplHtml, obj));
+					sel.html(ViewObjectTpl($tplHtml, obj));
 					
 					// bind/tag 갱신
 					initBind(sel);
@@ -424,7 +527,7 @@
 		if(tpl.attr("type") != "text/template") throw new Error("VOJS_CRITICAL_ERR: is not a template tag");
 		
 		var viewId = "_" + new Date().getTime() + "_",
-			tplHtml = _.template(tpl.html(), { viewId: viewId }),
+			tplHtml = ViewObjectTpl(tpl.html(), { viewId: viewId }),
 			targetSel = (targetId) ? $("#" + targetId) : $("body");
 		
 		if(tplHtml.indexOf("<script") != -1) throw new Error("VOJS_CRITICAL_ERR: script tag should not be used");
@@ -438,7 +541,7 @@
 	ViewObject.applyTo = function(obj, id) {
 		if(typeof(obj) != 'object') throw new Error("VOJS_CRITICAL_ERR: is not an object");
 		
-		_.extend(obj, new ViewObject(id));
+		$.extend(obj, new ViewObject(id));
 	}
 	
 	ViewObject.appendTo = function(obj, tplId, targetId) {
